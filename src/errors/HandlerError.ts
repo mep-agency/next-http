defee75f778ae60f6819af18855df3dc24e37ab5
Marco Lipparini <developer@liarco.net>
2024-03-20 16:58:37 +0100

    Using problem+json format for errors

--- src/errors/HandlerError.ts
+++ src/errors/HandlerError.ts
@@ -1,13 +1,33 @@
 import { NextResponse } from 'next/server';
 
+type ErrorParams = {
+  type?: string;
+  title?: string;
+  status?: number;
+  detail?: string;
+  instance?: string;
+  [extension: string]: any;
+};
+
 export default class HandlerError {
-  constructor(public readonly message: string = 'Internal server error', public readonly statusCode: number = 500) {}
+  constructor(public readonly params: ErrorParams) {
+    if (this.params.title === undefined) {
+      this.params.title = 'Internal server error';
+    }
+
+    if (this.params.status === undefined) {
+      this.params.status = 500;
+    }
+  }
 
   public response() {
-    return new NextResponse(this.message, { status: this.statusCode });
+    return new NextResponse(this.params.title, { status: this.params.status });
   }
 
   public jsonResponse() {
-    return NextResponse.json({ message: this.message }, { status: this.statusCode });
+    return NextResponse.json(this.params, {
+      status: this.params.status,
+      headers: { 'Content-Type': 'application/problem+json' },
+    });
   }
 }
